a55bd64ab34ed6187bdc656f78c796e5f1d70f55
falkenbeast <arshayush21@gmail.com>
2024-07-28 23:57:54 +0530

    number evenodd binary

--- ch7_p1.java
+++ ch7_p1.java
@@ -1,4 +1,4 @@
-import java.util.*;               //checks the number is even or not using and '&' operator 
+import java.util.*;               //checks the number is even or not using and '&' operator ....
 public class ch7_p1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
