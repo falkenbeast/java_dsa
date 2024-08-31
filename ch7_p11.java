e16e845b1fd984cca3b4b8da527644d7559c4c39
falkenbeast <arshayush21@gmail.com>
2024-08-31 17:18:04 +0530

    flipping an image

---
+++ ch7_p11.java
@@ -0,0 +1,18 @@
+//flipping an image
+
+
+public class ch7_p11 {
+    public int[][] flipAndInvertImage(int[][] image) {
+        for(int[] row : image) {
+            // reverse this array
+            for (int i = 0; i < (image[0].length + 1) / 2; i++) {
+                // swap
+                int temp = row[i] ^ 1;
+                row[i] = row[image[0].length - i - 1] ^ 1;
+                row[image[0].length - i - 1] = temp;
+            }
+        }
+        return image;
+    }
+}
+
