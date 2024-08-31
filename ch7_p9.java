e5c20b3d2bccd95b13fe6504efc75da72b7e3059
falkenbeast <arshayush21@gmail.com>
2024-08-31 17:17:34 +0530

    find xor of nos. from o to a

---
+++ ch7_p9.java
@@ -0,0 +1,28 @@
+
+// find xor of nos. from o to a 
+
+public class ch7_p9 {
+    public static void main(String[] args) {
+        int n = 234567;
+        System.out.println(Integer.toBinaryString(n));
+
+        System.out.println(setBits(n));
+    }
+
+    private static int setBits(int n) {
+        int count = 0;
+
+//        while (n > 0) {
+//            count++;
+//            n -= (n & -n);
+//        }
+
+        while (n > 0) {
+            count++;
+            n = n & (n-1);
+        }
+
+        return count;
+    }
+
+}
